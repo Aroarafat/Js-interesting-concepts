d2c03f9fea4138dbe13d9ad83ad433b0907020af
Aroarafat <arafathossainbcpsc@gmail.com>
2021-08-30 20:09:36 +0600

    quiz-answer-added

--- callback.js
+++ callback.js
@@ -25,3 +25,62 @@ document.getElementById('my-btn').addEventListener('click', handleClick)
 document.getElementById('btn').addEventListener('click', function () {
     console.log('buttn is clicked');
 })
+
+
+// QUIZ-PRACTICE-01
+
+// let p = 'JavaScript';
+// let q = p; p = 'React';
+// console.log(q);
+
+// QUIZ-PRACTICE-02
+
+// const isTrue = 'false';
+
+// if (!isTrue) {
+
+//     console.log('hello');
+
+// }
+
+// else {
+//     console.log('world');
+
+// }
+
+// QUIZ-PRACTICE-03
+// function sum(p, q) {
+
+//     console.log(p + q);
+
+// }
+
+// const result = sum(2, 3);
+
+// console.log(result);
+
+
+// QUIZ-PRACTICE-04
+
+// if ('2' === 2) {
+
+//     console.log('Inside if');
+
+// }
+
+// else {
+
+//     console.log('Inside else');
+
+// }
+
+// QUIZ-PRACTICE-05
+
+// function work(x, y = 4) {
+
+//     return x + y;
+
+// }
+
+// console.log(work(32);
+// 05 ER ANSWER SYNTAX ERROR
